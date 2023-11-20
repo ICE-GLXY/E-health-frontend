76cbca8eac7a24750a6cde19952a2aad573c926e
ICE-GLXY <taariqrlk@gmail.com>
2023-11-20 15:14:39 +0200

    small design changes

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Now UI Dashboard React - v1.5.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/now-ui-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/now-ui-dashboard-react/blob/main/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import Dashboard from "views/Dashboard.js";
 import Notifications from "views/Notifications.js";
 import Icons from "views/Icons.js";
@@ -30,6 +13,13 @@ var dashRoutes = [
     name: "Home",
     icon: "shopping_shop",
     component: <Dashboard />,
+    layout: "/admin",
+  },
+  {
+    path: "/extended-tables",
+    name: "User List",
+    icon: "files_paper",
+    component: <TableList />,
     layout: "/admin",
   },
   {
@@ -58,13 +48,6 @@ var dashRoutes = [
     name: "Nurse List",
     icon: "users_single-02",
     component: <Icons />,
-    layout: "/admin",
-  },
-  {
-    path: "/extended-tables",
-    name: "User List",
-    icon: "files_paper",
-    component: <TableList />,
     layout: "/admin",
   },
   {
